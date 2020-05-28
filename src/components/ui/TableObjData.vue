b7bde0b22e3c142ae7e3d23bf97f0e9f3103d9ea
Matt Perkins <hello@mattperkins.me>
2020-05-28 17:29:35 -0400

    pretty print data

--- src/components/ui/TableObjData.vue
+++ src/components/ui/TableObjData.vue
@@ -1,15 +1,47 @@
-<style lang="scss" scoped></style>
+<style lang="scss">
+    .string {
+        color: $grey8;
+    }
+
+    .number {
+        color: $grey8;
+    }
+
+    .boolean {
+        color: $indigo;
+    }
+
+    .null {
+        color: $grey6;
+    }
+
+    .key {
+        color: $purple;
+    }
+
+    .brace {
+        color: silver;
+    }
+
+    .none {
+        color: grey;
+    }
+
+</style>
 
 <template>
-        <table class="table zebra-rows zebra-cols auto compressed hover-rows">
-            <tbody>
-            <tr v-for="(key, kidx) in objectKeys" :key="kidx">
-                <th class="font-mono"><span class="lowercase text-secondary">{{key}}</span></th>
-                <td class="font-mono">{{getKeyValue(key)}}</td>
-                <td><span class="small">{{typeof data[key]}}</span></td>
-            </tr>
-            </tbody>
-        </table>
+    <table class="table top zebra-rows zebra-cols auto compressed hover-rows">
+        <tbody>
+        <tr v-for="(key, kidx) in objectKeys" :key="kidx">
+            <th class="font-mono"><span
+                    class="lowercase text-secondary">{{key}}</span></th>
+            <td>
+                <pre v-html="getKeyValue(key)"></pre>
+            </td>
+            <td><span class="small">{{typeof data[key]}}</span></td>
+        </tr>
+        </tbody>
+    </table>
 </template>
 
 <script>
@@ -18,10 +50,14 @@
     name      : 'TableObjData',
     components: {},
     props     : {
-      data: {
+      data   : {
         type    : Object,
         default : null,
         required: true
+      },
+      exclude: {
+        type   : Array,
+        default: []
       }
     },
     data() {
@@ -29,17 +65,38 @@
     },
     computed  : {
       objectKeys() {
-        return Object.keys(this.data);
+        return Object.keys(this.data).filter(key => this.exclude.indexOf(key) === -1);
       }
     },
     methods   : {
       getKeyValue(key) {
-        return JSON.stringify(this.data[key]);
-      }
+        return this.syntaxHighlight(JSON.stringify(this.data[key], null, 2));
+      },
+      syntaxHighlight(json) {
+        json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        // TODO, color braces
+        return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+          let cls = 'none';
+          if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+              cls = 'key';
+            } else {
+              cls = 'string';
+            }
+          } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+          } else if (/null/.test(match)) {
+            cls = 'null';
+          } else if (/[0-9]/.test(match)) {
+            cls = 'number'
+          }  else if (/(\[)?(\])?(\{)?(\})?/.test(match)) {
+            cls = 'brace'
+          }
+          return '<span class="' + cls + '">' + match + '</span>';
+        });
+      },
     },
     mounted() {
-      console.log(this.data);
-      console.log(this.objectKeys);
     }
   };
 </script>
